d7e42782c6c9bd9d0aebba20f94ca2c01dcc7adf
霧雨バニラ <118162831+kirisamevanilla@users.noreply.github.com>
2026-02-24 17:05:35 +0800

    wip

--- src/app/utils/getComboState.js
+++
@@ -1,7 +0,0 @@
-export default function getComboState(state) {
-  if (state == 0) return "";
-  if (state == 1) return "FC";
-  if (state == 2) return "FC+";
-  if (state == 3) return "AP";
-  if (state == 4) return "AP+";
-}
